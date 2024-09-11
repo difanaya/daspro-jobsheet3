4564aeadb59a0f63ddcc5cb1dc2c398c15ee9880
difanaya <difapolinema@gmail.com>
2024-09-11 14:25:55 +0700

    Contoh Variabel

---
+++ ContohVariabel10.java
@@ -0,0 +1,16 @@
+public class ContohVariabel10 {
+    public static void main(String[] args) {
+        String salahSatuHobySayaadalah = "Mendengarkan Musik" ;
+        boolean isPandai = true;
+        char jenisKelamin = 'P';
+        byte _umurSayaSekarang = 19 ;
+        double $ipk = 3.24, tinggi = 169;
+        System.out.println(salahSatuHobySayaadalah);
+        System.out.println("Apakah Pandai?" + isPandai);
+        System.out.println("Jenis Kelamin:" + jenisKelamin);
+        System.out.println("Umurku saat ini:" + _umurSayaSekarang);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+
+    }
+    
+}
